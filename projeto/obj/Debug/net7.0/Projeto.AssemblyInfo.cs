d9761dd2ff7e9726414f517329cf209b85e8921a
Felipe da Costa Ferreira <20231014040005@ifrn.local>
2024-11-04 07:56:35 -0300

    corrigindo conflitos

--- projeto/obj/Debug/net7.0/Projeto.AssemblyInfo.cs
+++ projeto/obj/Debug/net7.0/Projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e64a0bbacbebc0376ce10da5e3f3e9b53b5b76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feec4b44731067e16195589326a1bced22a17979")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
