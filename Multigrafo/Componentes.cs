34eabc64ed1052c027e8c3e16b1fe8dff701aacf
Felipe-CF <prog.study.fcf@gmail.com>
2025-01-25 16:34:32 -0300

    Criação dos componentes do grafo

---
+++ Multigrafo/Componentes.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+
+public class Vertice{
+    protected ArrayList arestas_in;
+    protected ArrayList arestas_out;
+    protected Object rotulo;
+
+    public Vertice(Object r){
+        
+        rotulo = r;
+        arestas_in = arestas_out = null;
+    }
+    public override string ToString() {
+        return rotulo.ToString();
+    }
+
+}
+
+public class Aresta{
+    protected Object rotulo;
+    protected Vertice vertice_in;
+    protected Vertice vertice_out;
+
+    public Aresta(Object r){
+        rotulo = r;
+        vertice_in = null;
+        vertice_out = null;
+    }
+    public Aresta(Object r, Vertice i, Vertice o){
+        rotulo = r;
+        vertice_in = i;
+        vertice_out = o;
+    }
+    public override string ToString() {
+        return rotulo.ToString();
+    }
+}
