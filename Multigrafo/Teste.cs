47ce71f249079a637fa8830298286c416f4a5b1c
Felipe-CF <prog.study.fcf@gmail.com>
2025-01-26 16:20:05 -0300

    feat: criação de Teste.cs para realização dos testes dos métodos implementados

---
+++ Multigrafo/Teste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Multigrafo;
+
+class Teste
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
