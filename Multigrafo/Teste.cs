26ec27c3ac13b71caab9c2a23f5e633e2669d27d
Felipe-CF <prog.study.fcf@gmail.com>
2025-02-02 22:38:04 -0300

    feat: correção e teste do metodo ehDirecionada()

--- Multigrafo/Teste.cs
+++ Multigrafo/Teste.cs
@@ -22,13 +22,10 @@ class Teste
         Aresta a4 = grafo.inserirArestaDirecionada(v5, v1, "a4");
         Aresta a5 = grafo.inserirArestaDirecionada(v2, v4, "a5");
 
+        bool a = grafo.ehDirecionada(a1);
+
         Object o = grafo.removeVertice(v4);
         Object m = grafo.removeVertice(v2);
-
-        // List<Aresta> arestas = grafo.GetArestas();
-
-        // arestas.Remove(a5);
-
 
         int x = 2;
         
